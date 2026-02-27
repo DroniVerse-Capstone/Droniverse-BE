09ce9b2a20a338a99db62d5094e4e6171b288a6a
theTrungg <trungthe200427@gmail.com>
2026-02-27 16:24:17 +0700

    Add abstractions IClock and ICurrentUser

--- Droniverse.Academy/Droniverse.Academy.Application/DTO/Request/CreateCourseRequestDto.cs
+++ Droniverse.Academy/Droniverse.Academy.Application/DTO/Request/CreateCourseRequestDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Droniverse.Academy.Domain.Entities;
+using Droniverse.Academy.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ Droniverse.Shared/Abstractions/ICurrentUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Droniverse.Shared.Abstractions
+{
+    public interface ICurrentUser
+    {
+        Guid UserId { get; }
+        bool IsAuthenticated { get; }
+    }
+}
