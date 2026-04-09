8fb185865a4f6520f14eb4ca8bda8b327798e8f2
Nguyễn Song Toàn <toannsse183104@fpt.edu.vn>
2026-04-09 19:17:57 +0700

    update : course of enrollment response

--- Droniverse.Academy/Droniverse.Academy.Application/Services/EnrollmentService.cs
+++ Droniverse.Academy/Droniverse.Academy.Application/Services/EnrollmentService.cs
@@ -184,6 +184,7 @@ public class EnrollmentService : IEnrollmentService
         {
             EnrollmentId = x.EnrollmentId,
             CourseId = x.CourseId,
+            CourseVersionId = x.CourseVersionId,
             CourseNameVN = x.CourseNameVN,
             CourseNameEN = x.CourseNameEN,
             ImageUrl = x.ImageUrl,
