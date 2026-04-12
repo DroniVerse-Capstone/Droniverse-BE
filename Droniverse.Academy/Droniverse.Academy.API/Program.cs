8ba4250a4a4bf3b988c1104d3a48be5f3730319e
ToTuyen <tuyentmse183888@fpt.edu.vn>
2026-04-12 23:48:21 +0700

    Update: swagger.json

--- Droniverse.Academy/Droniverse.Academy.API/Program.cs
+++ Droniverse.Academy/Droniverse.Academy.API/Program.cs
@@ -18,6 +18,8 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using Swashbuckle.AspNetCore.Filters;
 using Swashbuckle.AspNetCore.SwaggerUI;
+using System.IO;
+using System.Text;
 using System.Reflection;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -241,6 +243,7 @@ using (var scope = app.Services.CreateScope())
 // ======================
 
 app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+app.UseStaticFiles();
 
 //if (app.Environment.IsDevelopment())
 //{
@@ -260,6 +263,47 @@ app.UseSwaggerUI(c =>
     c.EnableFilter();
     c.ConfigObject.AdditionalItems["persistAuthorization"] = true;
 });
+
+// Inject swagger-custom.js into Swagger UI
+if (app.Environment.IsDevelopment())
+{
+    var swaggerCustomFile = Path.Combine(app.Environment.WebRootPath, "swagger-custom.js");
+    var swaggerCustomVersion = File.Exists(swaggerCustomFile)
+        ? File.GetLastWriteTimeUtc(swaggerCustomFile).Ticks.ToString()
+        : DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+
+    app.Use(async (context, next) =>
+    {
+        if (context.Request.Path.StartsWithSegments("/swagger"))
+        {
+            var originalBody = context.Response.Body;
+            using var memoryStream = new MemoryStream();
+            context.Response.Body = memoryStream;
+
+            await next();
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            var responseBody = await new StreamReader(memoryStream).ReadToEndAsync();
+
+            if (context.Response.ContentType?.Contains("text/html") == true)
+            {
+                responseBody = responseBody.Replace(
+                    "</body>",
+                    $"<script src=\"/swagger-custom.js?v={swaggerCustomVersion}\"></script></body>"
+                );
+            }
+
+            var modifiedBody = Encoding.UTF8.GetBytes(responseBody);
+            context.Response.Body = originalBody;
+            context.Response.ContentLength = modifiedBody.Length;
+            await context.Response.Body.WriteAsync(modifiedBody);
+        }
+        else
+        {
+            await next();
+        }
+    });
+}
 
 // app.UseHttpsRedirection();
 
