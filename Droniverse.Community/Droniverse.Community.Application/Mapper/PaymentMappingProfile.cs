a8bbdeab4d34d197efbb7ea1d1d9e0dfef7abf66
ToTuyen <tuyentmse183888@fpt.edu.vn>
2026-04-19 10:29:15 +0700

    Update PaymentMappingProfile.cs

--- Droniverse.Community/Droniverse.Community.Application/Mapper/PaymentMappingProfile.cs
+++ Droniverse.Community/Droniverse.Community.Application/Mapper/PaymentMappingProfile.cs
@@ -10,11 +10,12 @@ public class PaymentMappingProfile : Profile
     public PaymentMappingProfile()
     {
         CreateMap<Payment, PaymentResponseDto>()
+            .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => (Guid?)null))
             .ForMember(dest => dest.TransactionId, opt => opt.MapFrom(src => src.TransactionID))
             .ForMember(dest => dest.PaymentUrl, opt => opt.MapFrom(src => src.PaymentUrl))
             .ForMember(dest => dest.PaymentMethod, opt => opt.MapFrom(src => src.PaymentMethod))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.PaymentStatus))
             .ForMember(dest => dest.TransactionDate, opt => opt.MapFrom(src => src.TransactionDate))
-            
             ;
     }
 }
