ea42ed40757f4b741a2e47718133227b4b2d8d48
ToTuyen <tuyentmse183888@fpt.edu.vn>
2026-04-22 04:45:40 +0700

    Update: Done flow wallet & withdraw request for club manager

--- Droniverse.Community/Droniverse.Community.Application/DTO/Response/WithdrawResponseDto.cs
+++ Droniverse.Community/Droniverse.Community.Application/DTO/Response/WithdrawResponseDto.cs
@@ -6,8 +6,15 @@ namespace Droniverse.Community.Application.DTO.Response;
 
 public record WithdrawResponseDto
 {
-    public string Username { get; set; }
+    public Guid WithdrawID { get; set; }
     public decimal Amount { get; set; }
-
-
+    public WithdrawStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? ApprovedAt { get; set; }
+    public Guid RequesterID { get; set; }
+    public Guid? ApproverID { get; set; }
+    public string? Note { get; set; }
+    public string? RejectReason { get; set; }
+    public WalletResponseDto? Wallet { get; set; }
 }
---
+++ Droniverse.Community/Droniverse.Community.Infrastructure/Repositories/WithdrawRequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using Droniverse.Community.Domain.Entities;
+using Droniverse.Community.Domain.IRepository;
+using Droniverse.Community.Infrastructure.Persistence.MySql;
+
+namespace Droniverse.Community.Infrastructure.Repositories;
+internal class WithdrawRequestRepository : MySqlRepository<WithdrawRequest>, IWithdrawRequestRepository
+{
+    public WithdrawRequestRepository(MySqlDbContext context) : base(context)
+    {
+    }
+
+}
+
